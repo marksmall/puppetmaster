9992ce36d810c21eda927feb7d8f63eb2d1c3820
Mark Small <m.small@ed.ac.uk>
2014-04-24 15:14:16 +0100

    Added the Puppet master/agent nodes to be provisioned:
    We define 2 nodes, one represents the Puppet Master, the other the Puppet Agent connecting
    to the Master.  Very little is done here, we ensure Puppet is installed on both systems and
    that Puppet Master is installed on the master.  We still have to configure the master to
    accept requests from the client.  This should NOT be done automatically, this should just
    provision the machine, we should then connect to the master manually so certificates are
    correctly setup.

---
+++ manifests/site.pp
@@ -0,0 +1,29 @@
+#Quick Manifest to stand up a demo Puppet Master
+class base {
+#  include ssh
+  include puppet
+  include ohmyzsh
+
+  service { 'iptables':
+    ensure => 'stopped',
+  }
+  $vagrant = 'vagrant'
+  $root = 'root'
+
+  ohmyzsh::setup { "${vagrant}":
+    user => "${vagrant}",
+  }
+
+  ohmyzsh::setup { "${root}":
+    user => "${root}",
+  }
+}
+
+node 'puppetmaster.edina.ac.uk' {
+  include base
+  include puppet::master
+}
+
+node 'puppetagent.edina.ac.uk' {
+  include base
+}
