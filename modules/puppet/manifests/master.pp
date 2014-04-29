046fdd9c725bbe0be9ba0b8e93bfa30c10757034
Mark Small <m.small@ed.ac.uk>
2014-04-29 14:18:09 +0100

    Default to apache web server:
    Had planned to enable either Webrick or Apache Web Server but decided
    it was easier to always use Apache.  Webrick is used initially to
    generate any necessary certificates, we may want to include the certificates
    ourselves if necessary.

--- modules/puppet/manifests/master.pp
+++ modules/puppet/manifests/master.pp
@@ -1,6 +1,4 @@
 class puppet::master {
-  $apache = 'true'
-  notify { "Apache val is: ${apache}": }
   include puppet
   include puppet::params
 
@@ -8,130 +6,94 @@ class puppet::master {
     ensure => installed,
   }
 
-  if $apache == 'true' {
-    class {'passenger': }  
+  class {'passenger': }  
 
-    # class { 'apache':
-    #   default_mods  => false,
-    #   default_vhost => false,
-    #   mpm_module    => 'worker',
-    # }
+  # class { 'apache':
+  #   default_mods  => false,
+  #   default_vhost => false,
+  #   mpm_module    => 'worker',
+  # }
 
-    include apache
-    include apache::mod::deflate
-    include apache::mod::status
-    include apache::mod::mime
-    include apache::mod::headers
+  include apache
+  include apache::mod::deflate
+  include apache::mod::status
+  include apache::mod::mime
+  include apache::mod::headers
 
-    class { 'apache::mod::ssl':
-      ssl_compression => false,
-      ssl_options     => [ 'StdEnvVars' ],
-    }
-
-    $default_home = '/var/www/html'
-    apache::vhost { "${fqdn}":
-      port          => 80,
-      docroot       => $default_home,
-      serveradmin   => "root@$fqdn",
-      priority      => '14',
-      default_vhost => true,
-      directories   => [{
-        provider    => 'directory',
-        path        => $default_home,
-        allow       => "from all", # Fi this to real subnets.
-        deny        => 'from all',
-        order       => 'deny,allow',
-      }],
-    }
-
-    package { "mod_ssl":
-      ensure => present,
-    }
-
-    File {
-      owner => "root",
-      group => "root",
-      mode  => "0644",
-    }
-
-    $puppet_dir = '/var/www/html/puppetmasterd'
-    file { [ "${puppet_dir}",
-             "${puppet_dir}/production",
-             "${puppet_dir}/public",
-             "${puppet_dir}/tmp" ]:
-               ensure  => directory,
-               require => Package['puppet-server'],
-    }
-    
-    file { '/etc/httpd/conf.d/puppetmaster.conf':
-      ensure  => 'present',
-      content => template('puppet/puppetmaster.conf.erb'),
-      require => [
-        Package['mod_ssl'],
-        Exec['puppetmaster-run-once'],
-      ],
-      notify  => Service['httpd'],
-    }
-
-    file { "${puppet_dir}/config.ru":
-      ensure  => 'present',
-      owner   => 'puppet',
-      group   => 'puppet',
-      source  => '/usr/share/puppet/ext/rack/config.ru',
-      require => [
-                  Package['puppet-server'],
-                  File["${puppet_dir}"],
-                  ],
-      notify  => Service['httpd'],
-    }
-
-    file { '/etc/sysconfig/puppet':
-      ensure  => 'present',
-      content => template('puppet/etc/sysconfig/puppet.erb'),
-    }
-
-    
-    # service { "puppetmaster":
-    #   ensure     => stopped,
-    #   enable     => false,
-    #   hasrestart => true,
-    #   hasstatus  => true,
-    #   require    => [
-    #                  Package["puppet-server"],
-    #                  File["/etc/puppet/puppet.conf"],
-    #                  ],
-    # }
-
-    # Short of a nice way to get the certificates generated just start up
-    # puppetmaster and stop it again
-    exec { "puppetmaster-run-once":
-      command => "/sbin/service puppetmaster start && /sbin/service puppetmaster stop",
-      creates => "/var/lib/puppet/ssl/certs/puppet.${domain}.pem",
-      require => Service["puppetmaster"],
-    }
-
-    # service { "httpd":
-    #   ensure     => running,
-    #   enable     => true,
-    #   hasrestart => true,
-    #   hasstatus  => true,
-    #   require    => [
-    #     Package["httpd"],
-    #     Package["mod_ssl"],
-    #     Exec["puppetmaster-run-once"],
-    #   ],
-    # }
-
-    $ensure = 'stopped'
-    $enable = false
-  } else {
-    $ensure = 'running'
-    $enable = true
+  class { 'apache::mod::ssl':
+    ssl_compression => false,
+    ssl_options     => [ 'StdEnvVars' ],
   }
 
-  service { "puppetmaster":
-    ensure     => "${ensure}",
-    enable     => "${enable}",
+  $default_home = '/var/www/html'
+  apache::vhost { "${fqdn}":
+    port          => 80,
+    docroot       => $default_home,
+    serveradmin   => "root@$fqdn",
+    priority      => '14',
+    default_vhost => true,
+    directories   => [{
+      provider    => 'directory',
+      path        => $default_home,
+      allow       => "from all", # Fix this to use real subnets.
+      deny        => 'from all',
+      order       => 'deny,allow',
+    }],
+  }
+
+  File {
+    owner => "root",
+    group => "root",
+    mode  => "0644",
+  }
+
+  $puppet_dir = '/var/www/html/puppetmasterd'
+  file { [ "${puppet_dir}",
+           "${puppet_dir}/production",
+           "${puppet_dir}/public",
+           "${puppet_dir}/tmp" ]:
+    ensure  => directory,
+    require => Package['puppet-server'],
+  }
+  
+  file { '/etc/httpd/conf.d/puppetmaster.conf':
+    ensure  => 'present',
+    content => template('puppet/puppetmaster.conf.erb'),
+    require => [
+      Package['httpd'],
+      Exec['puppetmaster-run-once'],
+    ],
+    notify  => Service['httpd'],
+  }
+
+  file { "${puppet_dir}/config.ru":
+    ensure  => 'present',
+    owner   => 'puppet',
+    group   => 'puppet',
+    source  => '/usr/share/puppet/ext/rack/config.ru',
+    require => [
+      File["${puppet_dir}"],
+    ],
+    notify  => Service['httpd'],
+  }
+
+  file { '/etc/sysconfig/puppet':
+    ensure  => 'present',
+    content => template('puppet/etc/sysconfig/puppet.erb'),
+  }
+
+  # Short of a nice way to get the certificates generated just start up
+  # puppetmaster and stop it again
+  exec { "puppetmaster-run-once":
+    command => "/sbin/service puppetmaster start && /sbin/service puppetmaster stop",
+    creates => "/var/lib/puppet/ssl/certs/${fqdn}.pem",
+    require => Service["puppetmasterd"],
+  }
+
+
+  service { "puppetmasterd":
+    ensure     => 'stopped',
+    enable     => false,
     hasrestart => true,
     hasstatus  => true,
     require    => [
@@ -139,4 +101,18 @@ class puppet::master {
       File["/etc/puppet/puppet.conf"],
     ],
   }          
+
+  class { 'puppetdb':
+    require => Package[ 'puppetdb-terminus' ],
+  }
+  class { 'puppetdb::master::config': }
+  
+  exec { 'puppetdb ssl setup':
+    command => '/usr/sbin/puppetdb ssl-setup',
+    require => [
+      File['/etc/httpd/conf.d/puppetmaster.conf'],
+    ],
+    notify => Service[ 'puppetdb' ],
+  }
+
 }
